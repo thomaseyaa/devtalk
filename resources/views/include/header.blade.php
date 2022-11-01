00743544c56fcf1ab366afd4b28c511debf820b2
Thomas Eyaa <thomasfkey@gmail.com>
2022-11-01 15:07:56 +0100

    Default components :building_construction:

---
+++ resources/views/include/header.blade.php
@@ -0,0 +1,35 @@
+<!-- HEADER-->
+@if(session('user'))
+    <nav class="py-3 navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand py-0" href="/">
+                <h1 class="text-dark">None</h1>
+            </a>
+            <div class="collapse navbar-collapse" id="navbar-content">
+                <ul class="navbar-nav">
+                    <li class="nav-item mx-1"><a class="nav-link text-dark" href="/home">Accueil</a></li>
+                    <li class="nav-item mx-1"><a  href="{{ url('profile') }}" class="nav-link text-dark">Profil</a></li>
+                    <li class="nav-item mx-1"><a class="nav-link text-dark" href="/legal-notice">Mentions légales</a></li>
+                    <li class="nav-item mx-1"><a href="{{ url('disconnect') }}" class="nav-link text-dark">Déconnexion</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+@else
+    <nav class="py-3 navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand py-0" href="/">
+                <h1 class="text-dark">None</h1>
+            </a>
+            <div class="collapse navbar-collapse" id="navbar-content">
+                <ul class="navbar-nav">
+                    <li class="nav-item mx-1"><a class="nav-link text-dark" href="/">Accueil</a></li>
+                    <li class="nav-item mx-1"><a  href="{{ url('login') }}" class="nav-link text-dark">Connexion</a></li>
+                    <li class="nav-item mx-1"><a  href="{{ url('register') }}" class="nav-link text-dark">Inscription</a></li>
+                    <li class="nav-item mx-1"><a class="nav-link text-dark" href="/legal-notice">Mentions légales</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+@endif
+
---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Title-->
+    <title>Devtalk</title>
+    <!-- Google Fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500&amp;display=swap&amp;subset=latin-ext" rel="stylesheet">
+    <!-- Libs CSS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <!-- Style CSS-->
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+<body>
+<!-- Header-->
+@include('include.header')
+
+<!-- Body-->
+@yield('content')
+
+<!-- Footer-->
+@include('include.footer')
+</body>
+
