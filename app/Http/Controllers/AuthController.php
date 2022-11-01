5b9ed3a5c31bf5d0ed4deb850a6c65aeffec88f6
Thomas Eyaa <thomasfkey@gmail.com>
2022-11-01 15:05:52 +0100

    Register feature :sparkles:

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    // Register
+    public function register(Request $request){
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+        DB::table('users')->insert([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        session()->flash('message', [
+            'status' => 'success',
+            'text' => 'Compte créé'
+        ]);
+        return redirect('login');
+    }
+}
