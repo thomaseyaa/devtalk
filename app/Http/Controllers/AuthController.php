87c9b09a51f6ddcd49fae1a801d7c0359f0d8268
Thomas Eyaa <thomasfkey@gmail.com>
2022-11-03 00:20:15 +0100

    Cleaned AuthController :recycle:

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,16 +16,19 @@ class AuthController extends Controller
             'email' => 'required',
             'password' => 'required',
         ]);
+
         DB::table('users')->insert([
             'first_name' => $request->first_name,
             'last_name' => $request->last_name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
         session()->flash('message', [
             'status' => 'success',
             'text' => 'Compte créé'
         ]);
+        
         return redirect('login');
     }
 
@@ -37,31 +40,36 @@ class AuthController extends Controller
         ]);
 
         $user = DB::table('users')->where('email', $request->email)->first();
-       if ($user){
-           if (Hash::check($request->password, $user->password)){
-               session(['user' => $user]);
-               return redirect('home');
-           }
-           else{
-               session()->flash('message', [
-                   'status' => 'danger',
-                   'text' => 'Mauvais identifiant'
-               ]);
-               return redirect('login');
-           }
-       }
-       else{
+        
+        if ($user){
+            if (Hash::check($request->password, $user->password)){
+                session(['user' => $user]);
+                
+                return redirect('home');
+            }
+            else{
+                session()->flash('message', [
+                    'status' => 'danger',
+                    'text' => 'Mauvais identifiant'
+                ]);
+
+                return redirect('login');
+            }
+        }
+
+        else{
            session()->flash('message', [
                'status' => 'danger',
                'text' => 'Mauvais identifiant'
            ]);
            return redirect('login');
-       }
+        }
     }
 
     // Logout
     public function disconnect(){
         session()->forget('user');
+        
         return redirect('login');
     }
 }
