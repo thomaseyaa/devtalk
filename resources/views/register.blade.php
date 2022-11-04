4aae7701a119b986b1d3839ac1591f91df79846a
Thomas Eyaa <thomasfkey@gmail.com>
2022-11-04 01:42:03 +0100

    New register page :art:

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,8 +1,8 @@
 @extends('layout.default')
 
 @section('content')
-    <div class="pt-8 pb-16">
-        <div class="container">
+    <div class="pt-5 pb-16">
+        <div class="pt-5 container">
             <div class="row mt-8">
                 <div class="col-lg-6 col-md-10 m-auto">
                     <div class="row">
@@ -23,21 +23,21 @@
                     @endif
                         <form method="post" action='{{ url('register') }}'>
                             @csrf
-                            <div class="form-row">
-                                <div class="form-group col-lg-6 pr-2">
+                            <div class="row g-3 d-flex mb-3">
+                                <div class="input-group-lg col-lg-6 ">
                                     <input class="form-control rounded" type="text" name="first_name" placeholder="Prénom" value="{{ old('first_name') }}">
                                 </div>
-                                <div class="form-group col-lg-6 pl-2">
+                                <div class="input-group-lg col-lg-6 pl-2">
                                     <input class="form-control rounded" type="text" name="last_name" placeholder="Nom" value="{{ old('last_name') }}">
                                 </div>
                             </div>
-                            <div class="form-group mb-3">
+                            <div class="input-group-lg mb-3">
                                 <input class="form-control rounded" type="email" id="email" name="email" placeholder="Email" value="{{ old('email') }}">
                             </div>
-                            <div class="form-group mb-3">
+                            <div class="input-group-lg mb-3">
                                 <input class="form-control rounded" type="password" id="password" name="password" placeholder="Mot de passe">
                             </div>
-                            <button class="btn btn-primary text-white mb-4 w-100" type="submit">S'inscire</button>
+                            <button class="btn btn-lg btn-primary mb-4 w-100" type="submit">S'inscire</button>
                         </form>
                         <div class="mb-3">
                             <small>Déja inscrit ? <a href="/login">Connectez-vous !</a></small>
